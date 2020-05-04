e172bc43518281480d2b9e770fec6326c857e8f7
asasigure <mkmokymh@gmail.com>
2020-05-05 01:20:43 +0900

    add: image

--- src/component/KnowledgeCard.js
+++ src/component/KnowledgeCard.js
@@ -6,7 +6,11 @@ function KnowledgeCard({ knowledge }) {
   return (
     <div>
       <h2>名前: {user.name}</h2>
-      <img src={user.image} alt='user icon' />
+      <img
+        src={user.image}
+        style={{ width: "100px", height: "100px", objectFit: "cover" }}
+        alt='user icon'
+      />
       <p>created_at: {knowledge.created_at}</p>
       <p>カテゴリ: {knowledge.category}</p>
       <p>content: {knowledge.content}</p>
--- src/page/Signup.js
+++ src/page/Signup.js
@@ -1,26 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+
+const createObjectURL =
+  (window.URL || window.webkitURL).createObjectURL || window.createObjectURL;
+const initImage = "init_profile.png";
 
 function Signup() {
+  const [imageSrc, setImageSrc] = useState(initImage);
+
+  const handleChangeImageFile = (e) => {
+    console.log(e.target.files);
+
+    const imageFiles = e.target.files;
+    const image_url = createObjectURL(imageFiles[0]);
+    setImageSrc(image_url);
+  };
+
   return (
     <div className='App'>
-      <header className='l-header c-header'>
-        <div className='l-justify-spaceBetween c-header__content'>
-          <div className='c-header__logo'>
-            <span>時雨記</span>
-          </div>
-          <nav className='c-header__menu'>
-            <ul className='l-justify-left c-menu'>
-              <li className='c-menu__list'>トップ</li>
-              <li className='c-menu__list'>アバウト</li>
-              <li className='c-menu__list'>記事検索</li>
-            </ul>
-          </nav>
-        </div>
-      </header>
-      <div
-        style={{ width: "100%", height: 400, backgroundColor: "black" }}
-      ></div>
-      <p>Signup</p>
+      <div>
+        <input
+          type='file'
+          accept='image/*'
+          onChange={handleChangeImageFile}
+        ></input>
+        <img
+          src={imageSrc}
+          alt='登録するアカウント画像'
+          style={{ width: "100px", height: "100px", objectFit: "cover" }}
+        />
+        <label>name</label>
+        <input type='text' name='name'></input>
+        <label>id</label>
+        <input type='text' name='id'></input>
+        <label>password</label>
+        <input type='password' name='password'></input>
+        <button type='button'>登録</button>
+      </div>
     </div>
   );
 }
