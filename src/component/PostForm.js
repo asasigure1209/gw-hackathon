42ac4babcddeba5c42cf667db59ad91f7b9ec48f
asasigure <mkmokymh@gmail.com>
2020-05-05 21:17:05 +0900

    add: 投稿機能

--- src/component/PostForm.js
+++ src/component/PostForm.js
@@ -1,6 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import categories from "../constans/categories";
+import axios from "axios";
+
+const url = "http://localhost:3004/posts";
 
 function PostForm({ onClick }) {
+  const [content, setContent] = useState("");
+  const [category, setCategory] = useState(categories.etc);
+
+  const sendPostData = () => {
+    let params = new URLSearchParams();
+    // ID仮置
+    params.append("user_id", 1);
+    params.append("content", content);
+    params.append("category", category);
+
+    axios.post(url, params).then((res) => {
+      console.log("送信しました。");
+    });
+
+    onClick();
+  };
+
+  const handleChangeContent = (event) => {
+    setContent(event.target.value);
+  };
+
+  const handleChangeCategory = (event) => {
+    setCategory(event.target.value);
+  };
+
+  const isClick = content === "";
+
   return (
     <div>
       <img
@@ -9,14 +40,18 @@ function PostForm({ onClick }) {
         alt='自分のアイコン'
       />
       <h2>Daisuke Hasegawa</h2>
-      <select>
-        <option value='business'>ビジネススキル</option>
-        <option value='communication'>コミュニケーション</option>
-        <option value='mylife'>マイライフ</option>
-        <option value='etc'>つぶやき</option>
-      </select>
-      <input type='text'></input>
-      <button onClick={onClick}>投稿する</button>
+      <div>
+        <select value={category} onChange={handleChangeCategory}>
+          <option value={categories.business}>ビジネススキル</option>
+          <option value={categories.comminucation}>コミュニケーション</option>
+          <option value={categories.mylife}>マイライフ</option>
+          <option value={categories.etc}>つぶやき</option>
+        </select>
+        <textarea value={content} onChange={handleChangeContent}></textarea>
+        <button onClick={sendPostData} disabled={isClick}>
+          投稿する
+        </button>
+      </div>
     </div>
   );
 }
