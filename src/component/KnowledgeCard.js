8357732c4c9b6b46413a23ae13a3f20314eaf257
hasegawa.daisuke <h-2915@h-2915.lan>
2020-05-06 00:24:13 +0900

    card分からんすぎるので一旦

--- src/component/KnowledgeCard.js
+++ src/component/KnowledgeCard.js
@@ -4,17 +4,19 @@ function KnowledgeCard({ knowledge }) {
   const { user } = knowledge;
 
   return (
-    <div className='c-knowledgecard'>
+    <div className='c-knowledgecard clearfix::after'>
       <img
         src={user.image}
         style={{ width: "65px", height: "65px", objectFit: "cover" }}
         alt='user icon'
       />
-      <h2>名前: {user.name}</h2>
-      <p className='created'>created_at: {knowledge.created_at}</p>
-      <p className='contents'>content: {knowledge.content}</p>
-      <p className='category'>カテゴリ: {knowledge.category}</p>
-      <p className='useful'>役に立った: {knowledge.useful_count}</p>
+      <div className='c-knowledge'>
+        <p className='created'>{knowledge.created_at}</p>
+        <h2>{user.name}</h2>
+        <p className='contents'>{knowledge.content}</p>
+      </div>
+      <p className='category'>{knowledge.category}</p>
+      <p className='useful'>{knowledge.useful_count} 役に立った</p>
       <p className='good'>良いね: {knowledge.like_users.length}</p>
     </div>
   );
