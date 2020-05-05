d3e71757664f7da50bcda728bd424569263085a7
hasegawa.daisuke <h-2915@h-2915.lan>
2020-05-06 00:04:04 +0900

    add: card styling

--- src/component/KnowledgeCard.js
+++ src/component/KnowledgeCard.js
@@ -4,18 +4,18 @@ function KnowledgeCard({ knowledge }) {
   const { user } = knowledge;
 
   return (
-    <div>
-      <h2>名前: {user.name}</h2>
+    <div className='c-knowledgecard'>
       <img
         src={user.image}
-        style={{ width: "100px", height: "100px", objectFit: "cover" }}
+        style={{ width: "65px", height: "65px", objectFit: "cover" }}
         alt='user icon'
       />
-      <p>created_at: {knowledge.created_at}</p>
-      <p>カテゴリ: {knowledge.category}</p>
-      <p>content: {knowledge.content}</p>
-      <p>役に立った: {knowledge.useful_count}</p>
-      <p>良いね: {knowledge.like_users.length}</p>
+      <h2>名前: {user.name}</h2>
+      <p className='created'>created_at: {knowledge.created_at}</p>
+      <p className='contents'>content: {knowledge.content}</p>
+      <p className='category'>カテゴリ: {knowledge.category}</p>
+      <p className='useful'>役に立った: {knowledge.useful_count}</p>
+      <p className='good'>良いね: {knowledge.like_users.length}</p>
     </div>
   );
 }
