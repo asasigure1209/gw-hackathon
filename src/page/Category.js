d890c11e3b6e5ffda81a793beef04a32bea294be
hasegawa.daisuke <h-2915@h-2915.lan>
2020-05-06 04:04:00 +0900

    add:category logo

--- src/page/Category.js
+++ src/page/Category.js
@@ -18,6 +18,7 @@ function Category({ match }) {
       <div className='global'>
         <div className='l-contents clearfix'>
           <div className='l-header c-header'>
+            <img className='c-logo' src='/kip_logo.png'></img>
             <CategoryCardList />
             <LogoutButton />
           </div>
