1865539be58ff6d597023a7e727756d57c7e2d31
minami.hitoki <minami.hitoki@a-tm.co.jp>
2020-05-06 11:58:48 +0900

    add: Like and Useful

--- src/component/KnowledgeCard.js
+++ src/component/KnowledgeCard.js
@@ -1,8 +1,46 @@
-import React from "react";
+import React, { useContext } from "react";
 import { categoryName } from "../constans/categories";
+import axios from "axios";
+import { UserContext } from "../App";
+
+const likeUrl = "http://localhost:8000/likes";
+const usefulUrl = "http://localhost:8000/usefuls";
 
 function KnowledgeCard({ knowledge }) {
   const { user } = knowledge;
+  const [viewer, setViewer] = useContext(UserContext);
+
+  const sendLikeData = () => {
+    console.log(knowledge);
+
+    axios.post(likeUrl,{
+        user_id: viewer.id,
+        post_id: knowledge.id,
+      },{
+        headers: {
+          Authorization: `Bearer ${viewer.token}`
+      }}
+    ).then((res) => {
+      //再レンダリングを強制する
+      setViewer({...viewer});
+    });
+  };
+
+  const sendUsefulData = () => {
+    console.log(knowledge);
+
+    axios.post(usefulUrl,{
+        user_id: viewer.id,
+        post_id: knowledge.id,
+      },{
+        headers: {
+          Authorization: `Bearer ${viewer.token}`
+      }}
+    ).then((res) => {
+      //再レンダリングを強制する
+      setViewer({...viewer});
+    });
+  };
 
   return (
     <div className='c-knowledgecard clearfix'>
@@ -18,8 +56,8 @@ function KnowledgeCard({ knowledge }) {
       </div>
       <p className='category'>{categoryName[knowledge.category]}</p>
       <div className='c-right'>
-        <p className='useful'>{knowledge.useful_count} 役に立った</p>
-        <p className='good'>良いね: {knowledge.like_users.length}</p>
+        <button className='useful' onClick={sendUsefulData}>{knowledge.useful_count} 役に立った</button>
+        <button className='good' onClick={sendLikeData}>良いね: {knowledge.like_users}</button>
       </div>
     </div>
   );
